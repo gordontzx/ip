aab2fdd62eb81fc9459d5769faac3a72a6cef949
gordon <gordontaylol@gmail.com>
2025-08-20 16:17:39 +0800

    Add exit message

--- src/main/java/Dude.java
+++ src/main/java/Dude.java
@@ -1,6 +1,8 @@
 public class Dude {
     public static void main(String[] args) {
         System.out.println("Hello, I'm Dude");
-        System.out.println("What can I do for you?");
+        System.out.println("What can I do for you?\n");
+
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
