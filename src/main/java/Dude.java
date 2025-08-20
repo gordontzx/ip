b476bc04e5ea786228f33636eb860e14c117a5ad
gordon <gordontaylol@gmail.com>
2025-08-20 16:40:04 +0800

    Add echo feature

--- src/main/java/Dude.java
+++ src/main/java/Dude.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Dude {
     // Wraps message between horizontal lines and prints it
     private static void printMessage(String message) {
@@ -8,6 +10,19 @@ public class Dude {
 
     public static void main(String[] args) {
         printMessage("Hello! I'm Dude.\nWhat can I do for you?");
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print(": ");
+            String command = scanner.nextLine();
+
+            // User entered "bye", end program loop
+            if (command.equals("bye")) { break; }
+
+            // Temporarily echo user command
+            printMessage(command);
+        }
+        scanner.close();
 
         printMessage("Bye, Hope to see you again soon!");
     }
