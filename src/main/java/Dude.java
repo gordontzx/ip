a298f08e90724f653b23d257c54e0acbabdae93f
gordon <gordontaylol@gmail.com>
2025-08-20 16:30:46 +0800

    Add horizontal lines for messages

--- src/main/java/Dude.java
+++ src/main/java/Dude.java
@@ -1,8 +1,14 @@
 public class Dude {
-    public static void main(String[] args) {
-        System.out.println("Hello, I'm Dude");
-        System.out.println("What can I do for you?\n");
+    // Wraps message between horizontal lines and prints it
+    private static void printMessage(String message) {
+        System.out.println("--------------------------------------------------\n"
+                + message
+                + "\n--------------------------------------------------");
+    }
 
-        System.out.println("Bye. Hope to see you again soon!");
+    public static void main(String[] args) {
+        printMessage("Hello! I'm Dude.\nWhat can I do for you?");
+
+        printMessage("Bye, Hope to see you again soon!");
     }
 }
