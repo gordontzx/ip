3f1e710a6a304b61e814ae2d2d7b112074ca9941
gordon <gordontaylol@gmail.com>
2025-08-20 16:56:11 +0800

    Add support for adding and displaying user input

--- src/main/java/Dude.java
+++ src/main/java/Dude.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Dude {
+    private static ArrayList<String> list;
+
     // Wraps message between horizontal lines and prints it
     private static void printMessage(String message) {
         System.out.println("--------------------------------------------------\n"
@@ -8,8 +11,26 @@ public class Dude {
                 + "\n--------------------------------------------------");
     }
 
+    // Print list
+    private static void printList() {
+        StringBuilder sb = new StringBuilder();
+        if (list.isEmpty()) {
+            sb.append("List is empty!");
+        }
+
+        for (int i = 0; i < list.size(); ++i) {
+            sb.append(String.format("%d. %s", i+1, list.get(i)));
+            if (i != list.size() - 1)
+                sb.append('\n');
+        }
+        printMessage(sb.toString());
+    }
+
     public static void main(String[] args) {
         printMessage("Hello! I'm Dude.\nWhat can I do for you?");
+
+        // Initialize data structures
+        list = new ArrayList<>();
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
@@ -19,8 +40,12 @@ public class Dude {
             // User entered "bye", end program loop
             if (command.equals("bye")) { break; }
 
-            // Temporarily echo user command
-            printMessage(command);
+            if (command.equals("list")) {
+                printList();
+            } else {
+                list.add(command);
+                printMessage("Added: " + command);
+            }
         }
         scanner.close();
 
